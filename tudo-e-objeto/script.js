e4742e2981183246619d55889436e43b2c4aa9f3
miguelmendi <miguelmds925@gmail.com>
2022-11-08 18:29:03 -0300

    Aprendendo que tudo é objeto

---
+++ tudo-e-objeto/script.js
@@ -0,0 +1,25 @@
+var nome = "Miguel";
+
+var nomeMinusculo = nome.toLocaleLowerCase();
+
+var btn = document.querySelector(".btn");
+
+// nomeie 3 propriedades ou métodos de strings
+
+var nome = "Miguel";
+//fixed;
+//length;
+//slice;
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+
+var btn = document.querySelector(".btn");
+btn.appendChild;
+btn.append;
+btn.animate;
+btn.TEXT_NODE;
+btn.DOCUMENT_TYPE_NODE;
+console.log(btn);
+
+// busque na web um objeto (método) capaz de interagir com o clipboard,
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
